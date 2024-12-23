5ac5b8226119f84ac18cf346b1e407babe385a46
Namrathakaki987 <147963673+Namrathakaki987@users.noreply.github.com>
2024-12-23 18:21:45 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,15 +1,4 @@
-// import express from "express";
-// import { createTask, getAllTasks, getTasksForEvent,updateTaskStatus } from "../controllers/Task.js";
 
-// const router = express.Router();
-
-// // Create a new task
-// router.post("/", createTask);
-// router.get("/",getAllTasks)
-// // Get tasks for an event
-// router.get("/:eventId", getTasksForEvent);
-// router.patch("/:id", updateTaskStatus);
-// export default router;
 
 import express from "express";
 import {
@@ -25,11 +14,12 @@ const router = express.Router();
 router.route("/").post(createTask).get(getAllTasks);
 
 router.route("/:eventId").get(getTasksForEvent);
-router.route("/event/:eventId").get(getTasksForEvent);
 
-// Route for updating a task's status
 router.route("/:id").patch(updateTaskStatus);
 
 export default router;
+
+
+
 
 
