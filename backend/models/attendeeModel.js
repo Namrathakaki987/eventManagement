ced1f20c70e6c6afdeb5d4132ef077a7759e9af9
Namrathakaki987 <147963673+Namrathakaki987@users.noreply.github.com>
2024-12-23 18:20:56 +0530

    Update attendeeModel.js

--- backend/models/attendeeModel.js
+++ backend/models/attendeeModel.js
@@ -1,9 +1,13 @@
+
 import  mongoose  from "mongoose";
 
-const attendeeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
+ const attendeeSchema = new mongoose.Schema({
+ name: { type: String, required: true },
+     email: { type: String, required: true },
+     event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' },
     assignedTasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
-});
+ });
 
-export const Attendee = mongoose.model('Attendee', attendeeSchema);
+ export const Attendee = mongoose.model('Attendee', attendeeSchema);
+
+
